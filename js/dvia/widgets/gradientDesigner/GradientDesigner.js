fa03af6ed09faee57105595b0fb12a04b4630477
Tom Pryor <tom@dvia.com>
2012-01-02 13:42:43 -0500

    update app/widget to use dojo's new AMD module load pattern for better performance

--- js/dvia/widgets/gradientDesigner/GradientDesigner.js
+++ js/dvia/widgets/gradientDesigner/GradientDesigner.js
@@ -1,38 +1,34 @@
+define([
+	"dojo/_base/declare",	// declare
+	"dijit/_Widget",
+	"dojox/dtl/_Templated",
+	"dojo/fx/easing",
+	"dojo/NodeList-traverse"
+], function(declare, _Widget, _Templated){
 
-dojo.provide("dvia.widgets.gradientDesigner.GradientDesigner");
+/*=====
+	var _Widget = dijit._Widget;
+	var _TemplatedMixin = dijit._TemplatedMixin;
+=====*/
 
-//Always require resources before you try to use them. We're requiring these
-//two resources because they're part of our widget's inheritance hierarchy.
-dojo.require("dijit._Widget");
-dojo.require("dojox.dtl._Templated");
-dojo.require("dojo.fx.easing");
-dojo.require("dojo.NodeList-traverse");
+// module:
+//		dijit/form/HorizontalRule
+// summary:
+//		Hash marks for `dijit.form.HorizontalSlider`
 
-dojo.declare("dvia.widgets.gradientDesigner.GradientDesigner", [dijit._Widget, dojox.dtl._Templated], {
-	
-	// Logging variables
-	level: "ERROR",
-	methodName: null, // Only used for tracing, may be left out
-    toConsole: true,
-    toServerLog: true,
-		
+
+return declare("dvia.widgets.gradientDesigner.GradientDesigner", [_Widget, _Templated], {
+
 	_dijitTemplateCompat : true,
-	tiles : null,
-	_currentTile : null,
-	_targetTile : null,
-	_direction : "forward",
-
-	// this generate an error for some reason - when using dojo from google CDN, works fine when using a local copy of dojo!!!
-	// templateString: '<div><input dojoAttachEvent="onkeyup: keyUp"/><ul>{% for item in items %}<li>{{ item }} ${oldRepl}</li>{% endfor %}</ul></div>'
-	// templateString: '<div><input dojoAttachEvent="onkeyup: keyUp"/><ul>{% for item in items %}<li>{{ item }} - ${oldRepl}</li>{% endfor %}</ul></div>',
 	templateString : dojo.cache("dvia.widgets.gradientDesigner", "html/gradientDesigner.html"),
 	ef : dojo.fx.easing["quadInOut"],
+	
 	//Any initialization code would go here in the constructor. dijit._Widget and
 	//dijit._Templated do not have parameters in their constructors, so
 	//there wouldn't be any multiple-inheritance complications
 	//if you were to include some paramters here.
 	constructor : function() {
-		console.debug("1. in DTLexternal constructor");
+		console.debug("1. in GradientDesigner constructor");
 		//this.ef = dojo.fx.easing["quadInOut"];
 
 	},
@@ -40,7 +36,7 @@ dojo.declare("dvia.widgets.gradientDesigner.GradientDesigner", [dijit._Widget, d
 	//instantiation in buildRendering. This method is especially useful
 	//for manipulating the template before it becomes visible.
 	postMixInProperties : function() {
-		console.debug("2. in DTLexternal postMixInProperties");
+		console.debug("2. in GradientDesigner postMixInProperties");
 		// this.render();
 
 	},
@@ -48,16 +44,20 @@ dojo.declare("dvia.widgets.gradientDesigner.GradientDesigner", [dijit._Widget, d
 	//placed in the UI, but be warned that any child widgets contained
 	//in it may not be ready yet.
 	postCreate : function() {
-		console.debug("3. in DTLexternal postCreate");
-		this.startup();
+		console.debug("3. in GradientDesigner postCreate");
+		//this.startup();
 
 	},
 	//Called after the widget's children and all other widgets on the
 	//page have been created. Provides an opportunity to manipulate child
 	//widgets before they're displayed.
 	startup : function() {
-		console.debug("4. in DTLexternal startup");
+		console.debug("4. in GradientDesigner startup");
 		// this._getCurrentTile();
 
 	}
+
+
+});
+
 });
